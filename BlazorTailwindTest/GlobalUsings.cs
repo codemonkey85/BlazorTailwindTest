84e6a1eeb2fce4ddf417693d8525c0112d6298f4
Michael Bond <codemonkey8510@icloud.com>
2022-09-13 12:20:36 -0400

    Initial commit of project files

---
+++ BlazorTailwindTest/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using System.Net.Http.Json;
+global using BlazorTailwindTest;
+global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
