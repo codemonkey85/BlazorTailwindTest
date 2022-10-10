9e5848ec4388c3acff1d8e0d0064b485dbbb5bb2
Michael Bond <codemonkey8510@icloud.com>
2022-10-10 13:36:11 -0400

    Upgrade to .NET 7

--- BlazorTailwindTest/GlobalUsings.cs
+++ BlazorTailwindTest/GlobalUsings.cs
@@ -1,4 +1,3 @@
-﻿global using System.Net.Http.Json;
-global using BlazorTailwindTest;
+﻿global using BlazorTailwindTest;
 global using Microsoft.AspNetCore.Components.Web;
 global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
